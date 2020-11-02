8d3517bf28d669460909def6d83c550e33c4684a
George Mapaya <mambostudioz@gmail,com>
2020-11-02 14:49:33 -0600

    Refactor js

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ const hamburgerMenu = document.getElementById("hamburger-icon");
 const mobileNav = document.getElementById("nav-links");
 
 /* *****Functions***** */
+function displayMobileMenu() {
+  hamburgerMenu.className = "fas fa-times hamburger-icon";
+  hamburgerMenu.style.padding = "0.3rem 0.4rem";
+  mobileNav.classList.add("active");
+}
+
+function hideMobileMenu() {
+  hamburgerMenu.className = "fas fa-bars hamburger-icon";
+  hamburgerMenu.style.padding = "0.3rem";
+  mobileNav.classList.remove("active");
+}
 
 /* *****Event listeners***** */
 hamburgerMenu.addEventListener("click", () => {
   if (hamburgerMenu.className === "fas fa-bars hamburger-icon") {
-    hamburgerMenu.className = "fas fa-times hamburger-icon";
-    hamburgerMenu.style.padding = "0.3rem 0.4rem";
-    mobileNav.classList.add("active");
+    displayMobileMenu();
   } else {
-    hamburgerMenu.className = "fas fa-bars hamburger-icon";
-    hamburgerMenu.style.padding = "0.3rem";
-    mobileNav.classList.remove("active");
+    hideMobileMenu();
   }
 });
